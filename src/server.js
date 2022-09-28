cd77e59ae794124950464491b35b5453dbffc551
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-09-28 17:18:03 +0100

    Deleted mock data, connected database and server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const server = express();
 const { sanitize, content } = require("./templates.js");
-const mockData = require("./mockdata.js");
-const { listMusic } = require("./model/music.js");
+const { listMusic, updateMusicList, getUserId } = require("./model/music.js");
 
 const bodyParser = express.urlencoded();
 
-const postsArr = [...mockData];
+const postsArr = listMusic();
 
 server.get("/", (req, res) => {
   res.send(content(postsArr));
@@ -18,7 +17,19 @@ server.post("/", bodyParser, (req, res) => {
   const song = req.body.song;
   const genre = req.body.genre;
   const rating = req.body.rating;
-  postsArr.push({ username, artist, song, genre, rating });
+  // Adds user input into an array of objects
+  postsArr.push({ genre, artist, song, rating, username });
+  // check whether username exists
+  // function checkUsername => return true / false
+  // if (checkUsername) {
+  //   return;
+  // } else {
+  //   updateUserTable;
+  // };
+
+  const user_id = getUserId(username).id;
+  // Updates the music database with user input. Must use user_id because username doesn't feature on music table.
+  updateMusicList({ genre, artist, song, rating, user_id });
 
   res.redirect("/");
 });
