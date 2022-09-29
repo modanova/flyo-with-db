dc8d6d5075f0a351847397836dc87d304f0f3100
Abbbyccc <abbychien@hotmail.com>
2022-09-29 17:35:18 +0100

    tidy seeds data

--- test/four.test.js
+++ test/four.test.js
@@ -1,11 +1,12 @@
 const test = require("node:test");
 const assert = require("node:assert");
-const { updateUsers } = require("../src/model/music")
 const { listUsers } = require("../src/model/user")
+const { updateUsers } = require("../src/model/music")
+
 //test if username is inserted into user table properly 
 
 test("updateUsers update the table", () => {
     updateUsers('Abbie');
     const data = listUsers();
-    assert.equal(data.username, 'Abbie')
+    assert.equal(data[4].username, 'Abbie')
 })
